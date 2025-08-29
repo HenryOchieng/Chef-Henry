b962df14aa794e56daa8b94a54bc36aa6c89ef5d
Henry <henryochieng8@gmail.com>
2025-08-29 11:41:45 +0300

    scroll into view

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -4,19 +4,30 @@ import Ingredients from "./Ingredients"
 import { getRecipeFromMistral } from "./ai"
 
 export default function Form() {
-    const [ingredients, setMyIngredients] = React.useState([])
+    const [ingredients, setMyIngredients] = React.useState(["chicken", "all main spices", "corn", "coconut cream", "pasta"])
 
     const [recipe, setRecipe] = React.useState("")
+    const recipeSection = React.useRef(null)
+    console.log(recipeSection)
 
-    function addIngredient(formData) {
-        const newIngredient = formData.get("ingredient")
-        setMyIngredients(prevIngredient => [...prevIngredient, newIngredient]) 
-        console.log(newIngredient)
-    }
+    //Scroll into view when recipe is generated
+    React.useEffect(() =>{
+        if (recipe !== "" && recipeSection.current !== null) {
+            recipeSection.current.scrollIntoView({behavior: "smooth"})
+        }
+    }, [recipe])
 
+    //Get the recipe from the AI
     async function getRecipe(){
         const recipeMarkdown = await getRecipeFromMistral(ingredients)
         setRecipe(recipeMarkdown)
+    }
+
+    //Add ingredient to the list
+    function addIngredient(formData) {
+        const newIngredient = formData.get("ingredient")
+        setMyIngredients(prevIngredient => [...prevIngredient, newIngredient]) 
+        //console.log(newIngredient)
     }
     
     return (
@@ -32,6 +43,7 @@ export default function Form() {
             </form>
             { ingredients.length > 0 && 
                 <Ingredients 
+                    ref={recipeSection}
                     ingredients={ingredients} 
                     getRecipe={getRecipe}
                 />
